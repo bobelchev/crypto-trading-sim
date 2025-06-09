8dafa51bc1828ca7357af1d2d9ca568f768ae017
bobelchev <bobelchev2000@gmail.com>
2025-06-09 22:03:01 +0200

    add initial API Gateway service with basic route configuration

---
+++ gateway-crypto/gateway-crypto/src/test/java/com/example/gateway_crypto/GatewayCryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gateway_crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayCryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
