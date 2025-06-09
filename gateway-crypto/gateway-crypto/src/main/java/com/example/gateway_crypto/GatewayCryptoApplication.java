8d9261cceb4d89d4d93c6738f8ac9517e13c4e24
bobelchev <162323289+bobelchev@users.noreply.github.com>
2025-06-10 01:24:10 +0200

    Feature/api gateway (#8)
    * add initial API Gateway service with basic route configuration
    
    * add initial API Gateway service with basic route configuration
    
    * add CORS policy to gateway config
    
    * add route for ws market data
    
    * redo cors config using explicit config bean
    
    * update README

---
+++ gateway-crypto/gateway-crypto/src/main/java/com/example/gateway_crypto/GatewayCryptoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gateway_crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayCryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayCryptoApplication.class, args);
+	}
+
+}
