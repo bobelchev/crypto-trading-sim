aa36915e015f8052ec2236c4a72f0199691a10b1
bobelchev <bobelchev2000@gmail.com>
2025-06-11 22:12:11 +0200

    remove print log

--- market-data-streamer/market-data-streamer/src/main/java/com/example/marketdata/market_data_streamer/consumer/KafkaConsumerService.java
+++ market-data-streamer/market-data-streamer/src/main/java/com/example/marketdata/market_data_streamer/consumer/KafkaConsumerService.java
@@ -16,7 +16,7 @@ public class KafkaConsumerService {
 
     @KafkaListener(topics = "my_topic", groupId = "group_id")
     public void consume(String message) {
-        System.out.println("Message received: " + message);
+        //System.out.println("Message received: " + message);
         frontendWebSocketHandler.pushMarketData(message);
     }
 }
