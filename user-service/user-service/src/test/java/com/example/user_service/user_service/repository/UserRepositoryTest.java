e07ae299ef2ad9778a14c5c2aff5838ab9d8acf7
bobelchev <bobelchev2000@gmail.com>
2025-06-12 23:03:08 +0200

    copy and reconfigure unit test (excl. integration test of service)

---
+++ user-service/user-service/src/test/java/com/example/user_service/user_service/repository/UserRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.example.user_service.user_service.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+//https://www.baeldung.com/spring-jdbctemplate-testing#h2db
+//https://docs.spring.io/spring-boot/docs/2.1.1.RELEASE/reference/html/boot-features-testing.html#boot-features-testing-spring-boot-applications-testing-autoconfigured-jdbc-test
+@JdbcTest
+@Import(UserRepository.class)
+public class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    public static final long USERID = 1L;
+
+
+    /**
+     * Initial balance should be 10 000 as per requirement
+     */
+    @Test
+    public void testInitialBalance(){
+        assertEquals(new BigDecimal("10000.000000"), userRepository.getBalanceOfUser(USERID));
+    }
+
+    /**
+     * Tests if balance gets updated in the DB
+     */
+    @Test
+    public void testUpdatingBalance(){
+        userRepository.updateBalance(USERID, new BigDecimal("2000.000000"));
+        assertEquals(new BigDecimal("2000.000000"),userRepository.getBalanceOfUser(USERID));
+    }
+
+    /**
+     * Tests if negative balance will be accepted
+     */
+    @Test
+    public void testNegativeBalance(){
+        BigDecimal oldBalance = jdbcTemplate.queryForObject(
+                "SELECT balance FROM users WHERE id = ?",
+                        BigDecimal.class,
+                        USERID);
+        assertThrows(IllegalArgumentException.class, () -> {
+            userRepository.updateBalance(USERID, new BigDecimal("-2000.000000"));
+        });
+        BigDecimal newBalance = jdbcTemplate.queryForObject(
+                "SELECT balance FROM users WHERE id = ?",
+                BigDecimal.class,
+                USERID);;
+        //there shouldn't be a change
+        //additionally should throw an exception when implemented
+        assertEquals(oldBalance, newBalance);
+
+    }
+
+}
---
+++ user-service/user-service/src/test/java/com/example/user_service/user_service/service/UserServiceTest.java
@@ -0,0 +1,44 @@
+package com.example.user_service.user_service.service;
+
+
+import com.example.user_service.user_service.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class UserServiceTest {
+
+    UserRepository mockUserRepository;
+    UserService userService;
+
+    public static final BigDecimal DEFAULT_BALANCE = new BigDecimal("10000.000000");
+    public static final long USERID = 1L;
+    @BeforeEach
+    void setUp(){
+        mockUserRepository = mock(UserRepository.class);
+
+        userService = new UserService();
+        userService.userRepository = mockUserRepository;
+    }
+
+    @Test
+    public void testGetBalance(){
+        when(mockUserRepository.getBalanceOfUser(USERID)).thenReturn(new BigDecimal("10000.000000"));
+        BigDecimal result = userService.getBalance(USERID);
+        assertEquals(DEFAULT_BALANCE,result);
+        verify(mockUserRepository, times(1)).getBalanceOfUser(USERID);
+    }
+    @Test
+    public void testReset(){
+        userService.resetAccount(USERID);
+        verify(mockUserRepository, times(1)).resetBalance(USERID);
+        //verify here if calls are made
+        //verify(mockTxRepository,times(1)).deleteAllTxs(USERID);
+        //verify(mockHoldingService,times(1)).deleteAllHoldingsOfUser(USERID);
+    }
+
+}
