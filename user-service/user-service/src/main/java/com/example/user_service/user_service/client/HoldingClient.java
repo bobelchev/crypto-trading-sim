104bab23d7bd00d4889627708390f5f08f793aec
bobelchev <bobelchev2000@gmail.com>
2025-06-20 15:31:13 +0200

    integrate transaction-service into the rest of the microservices (microservice architecture achieved)

--- user-service/user-service/src/main/java/com/example/user_service/user_service/client/HoldingClient.java
+++ user-service/user-service/src/main/java/com/example/user_service/user_service/client/HoldingClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "crypto-trading-sim",contextId = "holdingClient")
+@FeignClient(name = "holding-service",contextId = "holdingClient")
 public interface HoldingClient {
     @DeleteMapping("/holdings/{userId}")
     void deleteAllUserHoldings(@PathVariable("userId") long userId);
--- user-service/user-service/src/main/java/com/example/user_service/user_service/client/TransactionClient.java
+++ user-service/user-service/src/main/java/com/example/user_service/user_service/client/TransactionClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="crypto-trading-sim",contextId = "transactionClient")
+@FeignClient(name="transaction-service",contextId = "transactionClient")
 public interface TransactionClient {
     @DeleteMapping("/transactions/{userId}")
     void deleteAllUserTransactions(@PathVariable("userId") long userId);
